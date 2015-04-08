216da160ad7dea2741f604ad4df1a3878bc44e02
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-04-08 10:47:36 +0100

    Add annotations

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  url         :string           not null
+#  title       :string           not null
+#  description :text
+#  identifier  :uuid
+#
+
 FactoryGirl.define do
   factory :page do
     url         { Faker::Internet.url } 
